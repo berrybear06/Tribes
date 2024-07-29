d3a2e531012223098023c270e1c84c2ce6016e98
berrybear06 <79681277+berrybear06@users.noreply.github.com>
2024-07-29 12:16:02 -0700

    Disallow promoting super units in boats
    Includes boats, ships, and battleships

--- src/core/actions/unitactions/MakeVeteran.java
+++ src/core/actions/unitactions/MakeVeteran.java
@@ -2,6 +2,9 @@ package core.actions.unitactions;
 
 import core.TribesConfig;
 import core.actions.Action;
+import core.actors.units.Battleship;
+import core.actors.units.Boat;
+import core.actors.units.Ship;
 import core.game.GameState;
 import core.actors.units.Unit;
 import core.Types;
@@ -21,6 +24,12 @@ public class MakeVeteran extends UnitAction
     public boolean isFeasible(final GameState gs) {
         Unit unit = (Unit) gs.getActor(this.unitId);
         if(unit.getType() == Types.UNIT.SUPERUNIT)
+            return false;
+        if (unit instanceof Boat && ((Boat) unit).getBaseLandUnit() == Types.UNIT.SUPERUNIT)
+            return false;
+        if (unit instanceof Ship && ((Ship) unit).getBaseLandUnit() == Types.UNIT.SUPERUNIT)
+            return false;
+        if (unit instanceof Battleship && ((Battleship) unit).getBaseLandUnit() == Types.UNIT.SUPERUNIT)
             return false;
         return unit.getKills() >= TribesConfig.VETERAN_KILLS && !unit.isVeteran();
     }
